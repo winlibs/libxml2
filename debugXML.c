ca4540f6dcc6cb583c0835dbd5ce4dd76d1d8741
Shivam Mathur <shivam_jpr@hotmail.com>
2026-05-04 18:12:46 +0530

    [CVE-2026-1757] shell: free cmdline before continue
    Backported from upstream commit 160c8a43ba37dfb07ebe6446fbad9d0973d9279d.

--- debugXML.c
+++ debugXML.c
@@ -2868,8 +2868,11 @@ xmlShell(xmlDocPtr doc, char *filename, xmlShellReadlineFunc input,
             command[i++] = *cur++;
         }
         command[i] = 0;
-        if (i == 0)
+        if (i == 0) {
+            free(cmdline);
+            cmdline = NULL;
             continue;
+        }
 
         /*
          * Parse the argument
