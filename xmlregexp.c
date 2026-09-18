1b7c9619b5f6c4b85926db3846e686ccc370ed41
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-18 13:42:21 +0530

    Fix CVE-2026-86137
    Backport upstream libxml2 bounds checking for the NXT macro in the regular expression parser.
    
    CVE-2026-86137: out-of-bounds read in xmlFAParsePosCharGroup through an unchecked NXT macro index.
    
    CWE: CWE-125
    
    CVSS: 3.1/AV:L/AC:H/PR:N/UI:N/S:U/C:N/I:N/A:L (2.9 LOW)
    
    Upstream-Commit: 76fe08d97de88bfaef2f7d5cd27f11954cc5bee2
    
    Upstream-Issue: https://gitlab.gnome.org/GNOME/libxml2/-/work_items/1099

--- xmlregexp.c
+++ xmlregexp.c
@@ -52,7 +52,9 @@
     xmlRegexpErrCompile(ctxt, str);
 #define NEXT ctxt->cur++
 #define CUR (*(ctxt->cur))
-#define NXT(index) (ctxt->cur[index])
+#define NXT(index)									\
+    (((size_t)(ctxt->cur + index - ctxt->string) < ctxt->len)				\
+      ? ctxt->cur[index] : 0)
 
 #define CUR_SCHAR(s, l) xmlStringCurrentChar(NULL, s, &l)
 #define NEXTL(l) ctxt->cur += l;
@@ -242,6 +244,7 @@ typedef xmlRegParserCtxt *xmlRegParserCtxtPtr;
 struct _xmlAutomata {
     xmlChar *string;
     xmlChar *cur;
+    size_t len;
 
     int error;
     int neg;
@@ -718,8 +721,14 @@ xmlRegNewParserCtxt(const xmlChar *string) {
     if (ret == NULL)
 	return(NULL);
     memset(ret, 0, sizeof(xmlRegParserCtxt));
-    if (string != NULL)
+    if (string != NULL) {
 	ret->string = xmlStrdup(string);
+	ret->len = strlen((const char *) ret->string);
+        if (ret->string == NULL) {
+            xmlFree(ret);
+            return(NULL);
+        }
+    }
     ret->cur = ret->string;
     ret->neg = 0;
     ret->negs = 0;
